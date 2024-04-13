f2bde1b815735d976adad8993d8f818e9fdcfa82
Piero Dotti <progiemmeh@gmail.com>
2024-04-13 11:46:56 +0200

    refactor(three_of_a_kind): getScoreByHighestValueByCount usage

--- typescript/src/Yatzy.ts
+++ typescript/src/Yatzy.ts
@@ -78,16 +78,8 @@ export default class Yatzy {
   }
 
   three_of_a_kind(): number {
-    const [d1, d2, d3, d4, d5] = this.dice;
-    var t;
-    t = [0, 0, 0, 0, 0, 0, 0, 0, 0];
-    t[d1 - 1]++;
-    t[d2 - 1]++;
-    t[d3 - 1]++;
-    t[d4 - 1]++;
-    t[d5 - 1]++;
-    for (let i = 0; i < 6; i++) if (t[i] >= 3) return (i + 1) * 3;
-    return 0;
+    const counts = this.countDiceByValue();
+    return this.getScoreByHighestValueByCount(counts, 3);
   }
 
   smallStraight(): number {

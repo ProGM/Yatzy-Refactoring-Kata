23d447abf8d45f438e64ddc4e1dcf26ef8794bf0
Piero Dotti <progiemmeh@gmail.com>
2024-04-13 12:03:21 +0200

    refactor: reuse method hasStraightFromTo

--- typescript/src/Yatzy.ts
+++ typescript/src/Yatzy.ts
@@ -74,16 +74,7 @@ export default class Yatzy {
   }
 
   largeStraight(): number {
-    const [d1, d2, d3, d4, d5] = this.dice;
-    var tallies;
-    tallies = [0, 0, 0, 0, 0, 0, 0, 0];
-    tallies[d1 - 1] += 1;
-    tallies[d2 - 1] += 1;
-    tallies[d3 - 1] += 1;
-    tallies[d4 - 1] += 1;
-    tallies[d5 - 1] += 1;
-    if (tallies[1] == 1 && tallies[2] == 1 && tallies[3] == 1 && tallies[4] == 1 && tallies[5] == 1) return 20;
-    return 0;
+    return this.hasStraightFromTo(MIN_DICE_VALUE + 1, MAX_DICE_VALUE) ? 20 : 0;
   }
 
   fullHouse(): number {
